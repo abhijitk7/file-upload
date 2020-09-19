9fbcab431ce9c5c6fcf1cc3b5dcfb20b8f34706b
Abhijit <abhikulkarni.1988@gmail.com>
2020-09-19 16:35:26 +0530

    Initial commit

---
+++ src/main/java/com/example/fileupload/controller/FileController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.example.fileupload.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author abhi
+ *
+ */
+@RestController
+public class FileController {
+
+}
